8febe750b6fb8d51b93c9e0cc30012d2084b6dfc
Smore1384 <161771408+Smore1384@users.noreply.github.com>
2024-12-16 21:52:27 -0400

    Add files via upload

---
+++ FirstApp/Form1.Designer.cs
@@ -0,0 +1,395 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace FirstApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void addpage() 
+        {
+            
+
+            // Create a ListBox
+            ListBox listBox = new ListBox
+            {
+                Location = new Point(10, 10),
+                Size = new Size(150, 100)
+            };
+
+            // Create a TextBox
+            TextBox textBox = new TextBox
+            {
+                Location = new Point(10, 120),
+                Size = new Size(150, 25)
+            };
+
+            // Create Button 1 (Add Button)
+            Button addButton = new Button
+            {
+                Text = "Add",
+                Location = new Point(10, 160),
+                Size = new Size(60, 30)
+            };
+
+            // Add functionality to Button 1
+            addButton.Click += (s, ev) =>
+            {
+                if (!string.IsNullOrEmpty(textBox.Text))
+                {
+                    listBox.Items.Add(textBox.Text);
+                    textBox.Clear();
+                }
+            };
+
+            // Create Button 2 (Clear Button)
+            Button clearButton = new Button
+            {
+                Text = "Clear",
+                Location = new Point(80, 160),
+                Size = new Size(60, 30)
+            };
+
+            // Add functionality to Button 2
+            clearButton.Click += (s, ev) =>
+            {
+                listBox.Items.Clear();
+            };
+
+ 
+        }
+      
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBoxpage = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Afficher";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 164);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(228, 95);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "   Devoir a complété   ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 265);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Supprimer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(315, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "               Note                ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(140, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Tag = "";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Devoir";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(160, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Date";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(315, 38);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(218, 221);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(161, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "JJ/MM/AA";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(670, 38);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(613, 88);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(218, 95);
+            this.listBox2.TabIndex = 13;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(683, 189);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Supprimer";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(613, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(216, 26);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "          Mes cours          ";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(683, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Afficher";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(910, 64);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(212, 161);
+            this.tabControl1.TabIndex = 18;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(204, 135);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(204, 135);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBoxpage);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(204, 135);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBoxpage
+            // 
+            this.textBoxpage.Location = new System.Drawing.Point(28, 68);
+            this.textBoxpage.Name = "textBoxpage";
+            this.textBoxpage.Size = new System.Drawing.Size(100, 20);
+            this.textBoxpage.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label7.Font = new System.Drawing.Font("Microsoft JhengHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(909, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(217, 26);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "          Mes notes          ";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Studdy Buddy";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label7;
+        private TextBox textBoxpage;
+    }
+}
---
+++ FirstApp/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FirstApp
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, List<int>> courseScores = new Dictionary<string, List<int>>();
+        private string tasksFilePath = "tasks.txt";
+        private string textboxFilePath = "text.txt";
+        private string devoirFilePath = "devoir.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadTasks();
+            LoadTasks2();
+            LoadTask3();
+            RemoveItemsNotNineChars();
+            tabControl1.TabPages.Clear();
+            foreach (var item in listBox2.Items)
+            {
+
+                TabPage tabPage = new TabPage();
+                tabPage.Text = item.ToString();
+
+
+                tabControl1.TabPages.Add(tabPage);
+                RichTextBox rtb = new RichTextBox();
+                rtb.Dock = DockStyle.Fill;
+                tabPage.Controls.Add(rtb);
+
+                //https://eng.mkforlag.com/articles/04_c_sharp_013
+
+                textBox4.Clear();
+            }
+
+
+
+
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) && string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Veuillez entrer un devoir et une date.");
+            }
+            else if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                if (!string.IsNullOrEmpty(textBox2.Text))
+                {
+                    string Merge = $"{textBox1.Text}, {textBox2.Text} ";
+                    listBox1.Items.Add(Merge);
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    RemoveItemsNotNineChars(); 
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez entrer une date.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez entrer un devoir.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner un devoir");
+            }
+        }
+
+        private void SaveTasks()
+        {
+            using (StreamWriter writer = new StreamWriter(devoirFilePath))
+            {
+                foreach (var item in listBox1.Items)
+                {
+                    writer.WriteLine(item.ToString());
+                }
+            }
+        }
+
+        private void LoadTasks()
+        {
+            if (File.Exists(devoirFilePath))
+            {
+                using (StreamReader reader = new StreamReader(devoirFilePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        listBox1.Items.Add(line);
+                    }
+                }
+            }
+        }
+
+        private void SaveTasks2()
+        {
+            using (StreamWriter writer = new StreamWriter(textboxFilePath))
+            {
+                writer.WriteLine(textBox3.Text);
+            }
+        }
+
+        private void LoadTasks2()
+        {
+            if (File.Exists(textboxFilePath))
+            {
+                using (StreamReader reader = new StreamReader(textboxFilePath))
+                {
+                    textBox3.Text = reader.ReadToEnd();
+                }
+            }
+        }
+        private void SaveTask3() 
+        {
+            using (StreamWriter writer = new StreamWriter(tasksFilePath))
+            {
+                foreach (var item in listBox2.Items)
+                {
+                    writer.WriteLine(item.ToString());
+                }
+            }
+        }
+        private void LoadTask3()
+        {
+            if (File.Exists(tasksFilePath))
+            {
+                using (StreamReader reader = new StreamReader(tasksFilePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        listBox2.Items.Add(line);
+                    }
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveTasks();
+            SaveTasks2();
+            SaveTask3();
+        }
+
+        private void RemoveItemsNotNineChars()
+        {
+            for (int i = listBox1.Items.Count - 1; i >= 0; i--)
+            {
+                string itemText = listBox1.Items[i].ToString();
+                string[] parts = itemText.Split(',');
+
+                if (parts.Length < 2)
+                {
+                    listBox1.Items.RemoveAt(i);
+                    continue;
+                }
+
+                string datePart = parts[1].Trim();
+                string pattern = @"^\d{2}/\d{2}/\d{2}$";
+
+                if (!System.Text.RegularExpressions.Regex.IsMatch(datePart, pattern))
+                {
+                    listBox1.Items.RemoveAt(i);
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox2.Text, "[^0-9/]"))
+            {
+                MessageBox.Show("Veuillez entrer dans le format présenté");
+                textBox2.Text = textBox2.Text.Remove(textBox2.Text.Length - 1);
+                textBox2.SelectionStart = textBox2.Text.Length;
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                listBox2.Items.Remove(listBox2.SelectedItem);
+                tabControl1.TabPages.Clear();
+                foreach (var item in listBox2.Items)
+                {
+
+                    TabPage tabPage = new TabPage();
+                    tabPage.Text = item.ToString();
+
+
+                    tabControl1.TabPages.Add(tabPage);
+
+                }
+
+                }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner une classe");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox4.Text))
+            {
+                listBox2.Items.Add(textBox4.Text);
+                textBox4.Clear();
+                tabControl1.TabPages.Clear();
+                foreach (var item in listBox2.Items)
+                {
+                    
+                    TabPage tabPage = new TabPage();
+                    tabPage.Text = item.ToString();
+                   
+
+                    tabControl1.TabPages.Add(tabPage);
+
+                    textBox4.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez entrer une de vos classe");
+            }
+
+
+        }
+    }
+}
